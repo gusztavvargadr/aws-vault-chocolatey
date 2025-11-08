69664b8e3fc6e8de82d33cf4b79d285e51d09ae0
Gusztáv Varga <mail@gusztavvargadr.me>
2025-11-08 10:24:27 +0100

    Try additional reverts

--- build/chef/cookbook.run.ps1
+++ build/chef/cookbook.run.ps1
@@ -1,5 +1,5 @@
 pushd ./src/chef/
-C:/opscode/chef/bin/chef-client.bat --minimal-ohai --override-runlist "chocolatey-package::default"
+C:/opscode/chef/bin/chef-client.bat --local-mode --minimal-ohai --override-runlist "chocolatey-package::default"
 popd
 
 exit $LASTEXITCODE
