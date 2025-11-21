1862f7aca5ba388e630e492790248b32492ecf89
Gusztáv Varga <mail@gusztavvargadr.me>
2025-11-21 23:21:40 +0100

    Get rid of Chef for build (#91)

--- src/chef/cookbooks/chocolatey-package/Policyfile.rb
+++
@@ -1,4 +0,0 @@
-name 'chocolatey-package'
-default_source :supermarket
-run_list 'chocolatey-package::default'
-cookbook 'chocolatey-package', path: '.'
--- src/chef/cookbooks/chocolatey-package/metadata.rb
+++
@@ -1,8 +0,0 @@
-name 'chocolatey-package'
-description 'Installs/Configures Chocolatey Package'
-
-license 'Unlicense'
-maintainer 'Gusztáv Varga'
-maintainer_email 'mail@gusztavvargadr.me'
-
-version '0.1.0'
